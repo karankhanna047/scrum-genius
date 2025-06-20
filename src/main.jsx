90aee58b7c102ad1da3bb7c6fc0d4efa32aec9c3
Karan <karan.khanna@ab-inbev.com>
2025-06-20 10:56:27 +0530

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics>
+    <Analytics />
 
     <App />
 
-    </Analytics>
   </StrictMode>,
 )
